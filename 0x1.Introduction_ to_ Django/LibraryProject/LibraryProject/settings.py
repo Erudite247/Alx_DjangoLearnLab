b471d918fbf0b82c91609a68f7fd30fc1b2bc4a7
Erudite247 <eruditesolomon@gmail.com>
2024-08-11 17:03:46 +0100

    Implementing and Interacting with Django Models

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xs%j-^*6tao1h(s_g!=7a)i!i5+aaj(6w+=-isaxi-ef!pc7iz'
+SECRET_KEY = 'django-insecure-&wc@c3ovp5@z_^=olt&j%sb_v_ti9!j)j+nr=xhvz_q^!l%cny'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
---
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/create.md
@@ -0,0 +1,10 @@
+
+
+from bookshelf.models import Book
+
+
+book = Book(title="1984", author="George Orwell", publication_year=1949)
+book.save()
+
+
+print(Book.objects.all())
