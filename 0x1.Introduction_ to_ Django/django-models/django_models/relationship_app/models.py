f395031da206f2fe211eb3bc388bd3fd206f12f0
Erudite247 <eruditesolomon@gmail.com>
2024-08-27 04:29:26 +0100

    task

--- 0x1.Introduction_ to_ Django/django-models/django_models/relationship_app/models.py
+++ 0x1.Introduction_ to_ Django/django-models/django_models/relationship_app/models.py
@@ -1,31 +1,27 @@
+# relationship_app/models.py
+
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField(default=2000)
 
-    def __str__(self):
-        return self.title
-
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
-    def __str__(self):
-        return self.name
-
-
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Replace '1' with a valid User ID or handle appropriately
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
