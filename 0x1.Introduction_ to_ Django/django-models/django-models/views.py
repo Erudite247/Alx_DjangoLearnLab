2c18eee6d2454b00c64bd4380786560b6de51872
Erudite247 <eruditesolomon@gmail.com>
2024-08-30 07:50:22 +0100

    task 1

--- 0x1.Introduction_ to_ Django/django-models/django-models/views.py
+++ 0x1.Introduction_ to_ Django/django-models/django-models/views.py
@@ -1,27 +1,15 @@
 # relationship_app/views.py
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Book, Library
 
-@permission_required('relationship_app.can_add_book')
-def add_book(request):
-    # Your logic for adding a book
-    pass
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    # Your logic for editing a book
-    pass
-
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    # Your logic for deleting a book
-    pass
-
+# Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
