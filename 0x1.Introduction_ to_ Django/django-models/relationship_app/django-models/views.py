72012418ac11467a8a206ec9b51506c9198c71b6
Erudite247 <eruditesolomon@gmail.com>
2024-08-30 07:35:45 +0100

    task 1

---
+++ 0x1.Introduction_ to_ Django/django-models/relationship_app/django-models/views.py
@@ -0,0 +1,27 @@
+# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from .models import Book, Library
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    # Your logic for adding a book
+    pass
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Your logic for editing a book
+    pass
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Your logic for deleting a book
+    pass
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
