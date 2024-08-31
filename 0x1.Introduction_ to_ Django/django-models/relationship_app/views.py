a4409db530cc68b443dcfd7e8af7b14056fa7586
Erudite247 <eruditesolomon@gmail.com>
2024-08-31 22:04:17 +0100

    tasks 1

--- 0x1.Introduction_ to_ Django/django-models/relationship_app/views.py
+++ 0x1.Introduction_ to_ Django/django-models/relationship_app/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
-from .models import Book
-from django.views.generic import DetailView
-from .models import Library
+# relationship_app/views.py
 
-def list_books(request):
-    books = Book.objects.all()
-    context = {
-        'books': books,
-    }
-    return render(request, 'list_books.html', context)
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from .models import Book, Library
 
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    # Your logic for adding a book
+    pass
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Your logic for editing a book
+    pass
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Your logic for deleting a book
+    pass
 
 class LibraryDetailView(DetailView):
     model = Library
