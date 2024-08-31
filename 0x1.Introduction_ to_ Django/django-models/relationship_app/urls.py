c7788fd80f180716d3e64ad313a123e5ee401f8c
Erudite247 <eruditesolomon@gmail.com>
2024-08-31 23:32:19 +0100

    task 1

--- 0x1.Introduction_ to_ Django/django-models/relationship_app/urls.py
+++ 0x1.Introduction_ to_ Django/django-models/relationship_app/urls.py
@@ -1,17 +1,7 @@
-# relationship_app/urls.py
-
 from django.urls import path
-from .views import add_book, edit_book, delete_book, LibraryDetailView
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
 ]
-
--- 0x1.Introduction_ to_ Django/django-models/relationship_app/views.py
+++ 0x1.Introduction_ to_ Django/django-models/relationship_app/views.py
@@ -1,41 +1,15 @@
-# relationship_app/views.py
+from django.shortcuts import render
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView, LogoutView
-
-
-
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
 
 
 
-
-@permission_required('relationship_app.can_add_book')
-def add_book(request):
-    # Your logic for adding a book
-    pass
-
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    # Your logic for editing a book
-    pass
-
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    # Your logic for deleting a book
-    pass
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
-    context_object_name = 'library'
-
-
-class RegisterView(CreateView):
-    template_name = 'relationship_app/register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
