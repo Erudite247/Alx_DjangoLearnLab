f880cea1b1a49b9688d0b2fbe209f89ad14dc8ad
Erudite247 <eruditesolomon@gmail.com>
2024-08-31 22:58:21 +0100

    task 33

--- 0x1.Introduction_ to_ Django/django-models/relationship_app/urls.py
+++ 0x1.Introduction_ to_ Django/django-models/relationship_app/urls.py
@@ -2,10 +2,16 @@
 
 from django.urls import path
 from .views import add_book, edit_book, delete_book, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView
 
 urlpatterns = [
     path('books/add/', add_book, name='add_book'),
     path('books/<int:pk>/edit/', edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', delete_book, name='delete_book'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
+
--- 0x1.Introduction_ to_ Django/django-models/relationship_app/views.py
+++ 0x1.Introduction_ to_ Django/django-models/relationship_app/views.py
@@ -4,6 +4,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from .models import Book, Library
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class RegisterView(CreateView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+
 
 @permission_required('relationship_app.can_add_book')
 def add_book(request):
