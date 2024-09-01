977f745d91fd2fef04022e1abac85e85353ca365
Erudite247 <eruditesolomon@gmail.com>
2024-09-01 01:11:20 +0100

    task 1

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# relationship_app/models.py
-from django.db import models
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -30,3 +26,4 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,45 +1,29 @@
-# relationship_app/query_samples.py
-import os
 import django
-
-# Set up Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")
 django.setup()
-
 from relationship_app.models import Author, Book, Library, Librarian
 
-def query_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)
-        print(f"Books by {author_name}:")
-        for book in books:
-            print(book.title)
-    except Author.DoesNotExist:
-        print(f"Author '{author_name}' does not exist.")
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    for book in books:
+        print(book.title)
 
-def list_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        books = library.books.all()
-        print(f"Books in {library_name}:")
-        for book in books:
-            print(book.title)
-    except Library.DoesNotExist:
-        print(f"Library '{library_name}' does not exist.")
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    for book in books:
+        print(book.title)
 
-def retrieve_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        librarian = Librarian.objects.get(library=library)
-        print(f"Librarian for {library_name}: {librarian.name}")
-    except Library.DoesNotExist:
-        print(f"Library '{library_name}' does not exist.")
-    except Librarian.DoesNotExist:
-        print(f"No librarian for '{library_name}'.")
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    print(librarian.name)
 
+# Sample usage
 if __name__ == "__main__":
-    # Example usage:
-    query_books_by_author("J.K. Rowling")
-    list_books_in_library("Central Library")
-    retrieve_librarian_for_library("Central Library")
+    books_by_author('Author Name')
+    books_in_library('Library Name')
+    librarian_for_library('Library Name')
