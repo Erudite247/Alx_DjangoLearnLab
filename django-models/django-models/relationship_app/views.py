f48d86f82f72e939cabfec5dfbff23bb2bc3c63c
Erudite247 <eruditesolomon@gmail.com>
2024-09-01 16:31:23 +0100

    taskkk

--- django-models/django-models/relationship_app/views.py
+++ django-models/django-models/relationship_app/views.py
@@ -1,8 +1,8 @@
-# relationship_app/views.py
-
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import permission_required
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
 from .models import Book, Library
 
 @permission_required('relationship_app.can_add_book')
@@ -22,7 +22,22 @@ def delete_book(request, pk):
     # Your logic for deleting a book
     pass
 
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('some_view')  # Redirect to some view after login
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
