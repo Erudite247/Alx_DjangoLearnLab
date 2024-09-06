44bfc14440705628d4f36615a312fd7abd3154fe
Erudite247 <eruditesolomon@gmail.com>
2024-09-06 06:57:50 +0100

    taskk 3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,18 +1,11 @@
 # api/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import BookViewSet
-from .views import BookList
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookViewSet
 
-# Router and BokViewSet registration
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'books', BookViewSet, basename='book')
 
-# API URLs to be determined by router
 urlpatterns = [
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('', include(router.urls)),
-    path('books/', BookList.as_view(), name='book_list'),
 ]
