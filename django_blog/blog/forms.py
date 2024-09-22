12cc25e479eabe41cc1d9387796b6a67f897157b
Erudite247 <eruditesolomon@gmail.com>
2024-09-22 18:30:45 +0100

    task 4

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -20,3 +20,22 @@ class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['content'].label = "Your Comment"
+from django import forms
+from .models import Post, Tag
+
+class TagWidget(forms.TextInput):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.attrs.update({'placeholder': 'Add tags (comma-separated)', 'class': 'tag-input'})
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tags'].widget.attrs.update({'placeholder': 'Add tags (comma-separated)'})
