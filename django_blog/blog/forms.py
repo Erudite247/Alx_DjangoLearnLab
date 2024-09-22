7cda0a77a10171896a85d1795671d3fac6ee3998
Erudite247 <eruditesolomon@gmail.com>
2024-09-22 18:02:30 +0100

    task 3

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,16 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Post
+from .models import Comment
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+class PostForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        model = Post
+        fields = ['title', 'content']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if commit:
-            user.save()
-        return user
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['content'].label = "Your Comment"
