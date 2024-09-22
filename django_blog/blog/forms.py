9cc42068322af10f1f2f663c8449c2de278ec26a
Erudite247 <eruditesolomon@gmail.com>
2024-09-22 18:24:55 +0100

    task 4

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,13 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, Tag
 from .models import Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']
 
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tags'].widget.attrs.update({'placeholder': 'Add tags (comma-separated)'})
 
 
 class CommentForm(forms.ModelForm):
