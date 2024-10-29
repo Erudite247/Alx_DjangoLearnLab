a8230006bcc36ac281210eff12355fe08db79fcc
Erudite247 <eruditesolomon@gmail.com>
2024-10-28 21:53:26 -0700

    tasks

---
+++ accounts/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
+        )
+        return user
